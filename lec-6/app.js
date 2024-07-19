79649086988e17b22a4886c2041c73cfd432fddc
mahmoudRiad22 <mahmoudriad027@gmail.com>
2024-07-19 22:27:59 +0300

    my work on lec 6 part1

---
+++ lec-6/app.js
@@ -0,0 +1,4 @@
+const request = require("request")
+
+///////////////  mapbox api  ////////////////////
+const geo_code_url = "https://api.mapbox.com/geocoding/v5/mapbox.places/egypt.json?access_token=pk.eyJ1IjoiaXNsYW0yODQiLCJhIjoiY2wwamEzNmFhMGFtNTNkb3pqaXk4bXNnYSJ9.qYlrWIqo41gXgNNc4h8yIw"
